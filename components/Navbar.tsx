9aa4c5411f4387f7f977ed46373f96a623f2614d
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-09-19 23:11:55 +0530

    navbar v1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,23 @@ const Navbar = () => {
             <div className='flex justify-center'>
                 <img height={150} width={150} src="https://testimonial.to/static/media/logo.5ff3c18e.svg" alt="" />
             </div>
-            <div>
+            <div className='flex gap-8 text-[#d9e3ea] font-bold'>
+                <div className='flex align-center'>
+                    <div>
 
+                        Customers
+                    </div>
+                    <div>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </div>
+
+                </div>
+                <div className='cursor-pointer'>Features</div>
+                <div className='cursor-pointer'>Integrations</div>
+                <div className='cursor-pointer'>Pricing </div>
             </div>
             <div className='flex justify-between items-center'>
                 <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Sign in</button>
