8169c308ee571c0541427db94f45b9cc3112d087
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-09-22 01:03:52 +0530

    navbar v1

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+            hi from dashboard.
+        </div>
+    )
+}
+
+export default Dashboard
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 "use client"
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+    const session = useSession();
+    const router = useRouter();
     return (
         <div className=' flex justify-between p-5 text-white items-center'>
             <div className='flex justify-center'>
@@ -27,10 +30,19 @@ const Navbar = () => {
                 <div className='cursor-pointer'>Pricing </div>
             </div>
             <div className='flex justify-between items-center'>
-                <button onClick={() => signIn()} type="button" className="text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Sign in</button>
-                <button
-                    onClick={() => signIn()}
-                    type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</button>
+                {session.data?.user &&
+                    <div className='flex justify-between items-center'>
+                        <button onClick={() => signOut()} type="button" className="text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Log out</button>
+                        <img src={session.data?.user?.image ?? ""} className='rounded-full' height={30} width={30} alt="" />
+                    </div>
+                }
+                {!session.data?.user && <>
+
+                    <button onClick={() => { console.log('hiii'); signIn() }} type="button" className="text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Sign in</button>
+                    <button
+                        onClick={() => { signIn() }}
+                        type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</button>
+                </>}
             </div>
         </div>
     )
